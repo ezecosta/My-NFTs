14e67fd70fcf8c73c74e6303bfee4ab2af663dfe
Ezequiel <64994705+ezecosta@users.noreply.github.com>
2023-06-21 00:27:55 -0300

    Adding finalized project to repo
    Final basic project on NFTs. Built on Solidity using ERC-721 standard.

---
+++ ezNFTs.sol
@@ -0,0 +1,34 @@
+//Contract based on [https://docs.openzeppelin.com/contracts/3.x/erc721](https://docs.openzeppelin.com/contracts/3.x/erc721)
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+contract MyNFT is ERC721URIStorage {
+    uint private _tokenIds;
+    address payable owner;
+    uint price;
+
+    modifier onlyOwner(){
+        require(msg.sender == owner, "Only the owner can authorize users.");
+        _;
+    }
+
+    constructor(uint _price) ERC721("MyNFT", "NFT") {
+        owner = payable(msg.sender);
+        price = _price;
+    }
+
+    //permite asignarle ese nft a otro usuario dentro de la blockchain
+    //we're using the onlyOwner modifier for this, could be removed
+    function mintNFT(address recipient, string memory tokenURI) public onlyOwner payable returns (uint256)
+    {
+        _tokenIds = _tokenIds + 1;
+        _mint(recipient, _tokenIds);
+        _setTokenURI(_tokenIds, tokenURI);
+
+        return _tokenIds;
+    }
+}
